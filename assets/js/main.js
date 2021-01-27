314a1d8a00c5a6a3c0b448f66229eeb9f5cd6caf
KurakulaBhanuTeja <bhanuteja377@gmail.com>
2021-01-27 12:02:30 +0530

    3

--- assets/js/main.js
+++ assets/js/main.js
@@ -145,23 +145,6 @@
       once: true
     });
   }
-
-  // Porfolio isotope and filter
-  $(window).on('load', function() {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
-    });
-
-    $('#portfolio-flters li').on('click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-
-      portfolioIsotope.isotope({
-        filter: $(this).data('filter')
-      });
-      aos_init();
-    });
-
     // Initiate venobox (lightbox feature used in portofilo)
     $('.venobox').venobox({
       'share': false
@@ -170,22 +153,6 @@
     // Initiate aos_init() function
     aos_init();
 
-  });
-
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
-
-  // Portfolio details carousel
-  $(".portfolio-details-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
   });
 
 })(jQuery);
